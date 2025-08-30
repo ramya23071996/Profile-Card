d6b2173566ef6bc80b6c7a81b2c80251845c7b89
ramya23071996 <ramyasowmiya96@gmail.com>
2025-08-30 18:27:18 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main>
       <h1 style={{ textAlign: "center" }}>Profile Card App</h1>
-      <ProfileCard name="Vanishree" age="24" location="Bangalore" />
+      <ProfileCard name="Joni" age="24" location="Bangalore" />
       <ProfileCard name="John Doe" age="30" location="New York" />
     </main>
   );
